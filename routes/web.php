31cd5c1792f42f5cf15bd7b077a045ceffe2db80
Marius Jakobi <marius.jakobi@googlemail.com>
2020-12-08 16:20:59 +0100

    Grouped customer routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
@@ -26,4 +27,9 @@ Route::post('/auth/logout', [AuthController::class, 'logout'])->name('auth.logou
 
 Route::middleware(['auth'])->group(function() {
     Route::get('/dashboard', [DashboardController::class, 'showDashboard'])->name('dashboard');
+
+    Route::prefix('customers')->group(function() {
+        Route::get('/', [CustomerController::class, 'list'])->name('customers.list');
+        Route::get('/{id}', [CustomerController::class, 'details'])->name('customer.details');
+    });
 });
