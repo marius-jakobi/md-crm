b6a705204f030e6ed7a69e6059c9aebb5913c926
Marius Jakobi <marius.jakobi@googlemail.com>
2020-12-10 15:23:41 +0100

    show addresses

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -6,4 +6,24 @@
     <h1>{{ $customer->name }}</h1>
     <p>erstellt: {{ $customer->created_at }}</p>
     <p>zuletzt geändert: {{ $customer->updated_at }}</p>
+    <h2>Rechnungsadressen</h2>
+    <ul>
+    @foreach($customer->billingAddresses as $address)
+        <li>
+            {{ $address->name }},
+            {{ $address->po_box ? 'Postfach ' . $address->po_box : $address->street }},
+            {{ $address->zip . ' ' . $address->city }}
+        </li>
+    @endforeach
+    </ul>
+    <h2>Lieferadressen</h2>
+    <ul>
+    @foreach($customer->shippingAddresses as $address)
+        <li>
+            {{ $address->name }},
+            {{ $address->po_box ? 'Postfach ' . $address->po_box : $address->street }},
+            {{ $address->zip . ' ' . $address->city }}
+        </li>
+    @endforeach
+    </ul>
 @endsection
