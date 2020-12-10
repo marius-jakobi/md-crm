bf0d9931f7643a8871a5a764e1078bc0f04ebfdc
Marius Jakobi <marius.jakobi@googlemail.com>
2020-12-10 11:03:30 +0100

    Authorize admins

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::before(function ($user) {
+            if ($user->hasRole('administrators')) {
+                return true;
+            }
+        });
     }
 }
