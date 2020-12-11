ba05c459fd647a7b30889106eb306557028d8b9f
Marius Jakobi <marius.jakobi@googlemail.com>
2020-12-11 12:44:24 +0100

    Show amount of addresses

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -8,6 +8,8 @@
         <thead>
         <tr>
             <th>Name</th>
+            <th>Rechnungsadressen</th>
+            <th>Lieferadressen</th>
             <th>erstellt</th>
         </tr>
         </thead>
@@ -22,6 +24,8 @@
                     {{ $customer->name }}
                     @endcannot
                 </td>
+                <td>{{ $customer->billingAddresses->count() }}</td>
+                <td>{{ $customer->shippingAddresses->count() }}</td>
                 <td>{{ $customer->created_at }}</td>
             </tr>
         @endforeach
