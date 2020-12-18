e9b2cb15e9cfb1910721ae820302a170c2cd0877
Marius Jakobi <marius.jakobi@googlemail.com>
2020-12-18 14:00:30 +0100

    Removed unused import

--- tests/Unit/Http/Controllers/AuthControllerTest.php
+++ tests/Unit/Http/Controllers/AuthControllerTest.php
@@ -5,7 +5,6 @@ namespace Tests\Unit\Http\Controllers;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class AuthControllerTest extends TestCase
